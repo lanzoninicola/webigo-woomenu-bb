2be3f6c96a5a42a44b4019e0dabb778723d7304c
Nicola <lanzoni.nicola@gmail.com>
2022-05-25 15:52:04 -0300

    Logic, style, and js from the production site

--- includes/class-webigo-shortcode-woomenu.php
+++ includes/class-webigo-shortcode-woomenu.php
@@ -62,10 +62,94 @@ class Webigo_Shortcode_Woomenu
     public function woomenu_shortcode($atts)
     {
         $atts = shortcode_atts(array(), $atts, $this->shortcode_name);
-?>
 
-        <h1 id="helloworld">Hello World</h1>
+        $taxonomy     = 'product_cat';
+        $orderby      = 'menu_order';
+        $show_count   = 0;      // 1 for yes, 0 for no
+        $pad_counts   = 0;      // 1 for yes, 0 for no
+        $hierarchical = 1;      // 1 for yes, 0 for no
+        $title        = '';
+        $empty        = 0;
 
-<?php
+        $show_sub_cat_image = 0;
+
+        $uncategorized_label = 'Uncategorized';
+
+        $args = array(
+            'taxonomy'     => $taxonomy,
+            'orderby'      => $orderby,
+            'show_count'   => $show_count,
+            'pad_counts'   => $pad_counts,
+            'hierarchical' => $hierarchical,
+            'title_li'     => $title,
+            'hide_empty'   => $empty
+        );
+        $all_categories = get_categories($args);
+
+        echo '<nav class="product-cats-nav-menu bricks-nav-menu-wrapper" data-mobile-visibility="hidden">';
+
+        echo '<i class="ion-md-close" data-mobile-visibility="hidden"></i>'; // close mobile menu
+
+        echo '<ul class="product-cats-nav bricks-nav-menu">';
+
+        foreach ($all_categories as $cat) {
+
+            if ($cat->category_parent == 0) {
+                $category_id = $cat->term_id;
+
+                if ($cat->name != $uncategorized_label) {
+
+                    echo '<li class="product-cat menu-item" data-entity="product_cat" data-product-cat="' . $cat->term_id . '">';
+                    // get the link of category
+                    echo '<div>' . $cat->name . '</div>';
+
+                    // extracting the subcategories 
+                    $args2 = array(
+                        'taxonomy'     => $taxonomy,
+                        'child_of'     => 0,
+                        'parent'       => $category_id,
+                        'orderby'      => $orderby,
+                        'show_count'   => $show_count,
+                        'pad_counts'   => $pad_counts,
+                        'hierarchical' => $hierarchical,
+                        'title_li'     => $title,
+                        'hide_empty'   => $empty
+                    );
+
+                    $sub_cats = get_categories($args2);
+
+                    echo '<ul class="product-subcats bricks-nav-menu" data-entity="product_subcat" data-product-cat="' . $category_id . '" data-visibility="hidden">';
+
+                    echo '<li class="product-subcat all menu-item ">';
+                    echo '<a href="' . get_term_link($cat->slug, 'product_cat') . '"> Todos ' . $cat->name . '</a>';
+                    echo '</li>';
+
+                    if ($sub_cats) {
+                        foreach ($sub_cats as $sub_category) {
+
+                            echo '<li class="product-subcat menu-item">';
+
+                            // get the link of sub-category
+                            echo '<a href="' . get_term_link($sub_category->slug, 'product_cat') . '">' . $sub_category->name . '</a>';
+
+                            // sub category image
+                            if ($show_sub_cat_image === 1) {
+                                $thumbnail_id = get_term_meta($sub_category->term_id, 'thumbnail_id', true);
+                                $image = wp_get_attachment_url($thumbnail_id);
+                                echo  '<img src="' . $image . '" alt="" height="20" width="20">';
+                                //add other code here to display child details
+                            }
+
+                            echo '</li>'; // closing sub-category single
+                        }
+                    }
+                    echo '</ul>'; // closing sub-categories
+                    echo '</li>'; // closing product-cat  
+                }
+            }
+        }
+        echo '</ul>';
+        echo '</nav>'; // closing product-cats-wrapper
+
     }
 }
--- public/js/webigo-woomenu-public.js
+++ public/js/webigo-woomenu-public.js
@@ -1,3 +1,83 @@
 (function (d) {
   "use strict";
+
+  const catMenuItems = d.querySelectorAll(
+    ".product-cats-nav-menu li.product-cat"
+  );
+  const subCatMenuWrappers = d.querySelectorAll(
+    ".product-cats-nav-menu li.product-cat .product-subcats"
+  );
+
+  let catIdTargeted = false; // int
+  let subcatIdTargeted = false; // int
+  let subcatElTargeted = false; // element object
+  let subcatElTargetedVisibilityStatus = "hidden"; // hidden ,  shown
+
+  catMenuItems.forEach((item) => {
+    item.addEventListener("mouseover", showSubMenu);
+  });
+
+  subCatMenuWrappers.forEach((item) => {
+    item.addEventListener("mouseleave", hideSubMenu);
+  });
+
+  function showSubMenu() {
+    const el = this;
+
+    if (subcatElTargeted) {
+      hideSubMenu();
+    }
+
+    catIdTargeted = el.getAttribute("data-product-cat");
+    subcatIdTargeted = catIdTargeted;
+    subCatMenuWrappers.forEach((subCatItem) => {
+      if (subCatItem.getAttribute("data-product-cat") == subcatIdTargeted) {
+        subcatElTargeted = subCatItem;
+      }
+    });
+    subcatElTargetedVisibilityStatus = "shown";
+
+    if (subcatElTargetedVisibilityStatus === "shown" && subcatElTargeted) {
+      subcatElTargeted.setAttribute("data-visibility", "shown");
+    }
+  }
+
+  function hideSubMenu() {
+    if (subcatElTargeted) {
+      subcatElTargeted.setAttribute("data-visibility", "hidden");
+    }
+  }
+
+  // mobile menu management
+  const navMenuMobile = d.querySelectorAll(".product-cats-nav-menu")[0];
+
+  const closeMobileMenuButton = d.querySelectorAll(
+    ".product-cats-nav-menu .ion-md-close"
+  )[0];
+
+  const openMobileMenuButton = d.getElementById("hamburger-mobile-menu");
+
+  if (closeMobileMenuButton) {
+    closeMobileMenuButton.addEventListener("click", hideMobileMenu);
+  }
+
+  if (openMobileMenuButton) {
+    openMobileMenuButton.addEventListener("click", showMobileMenu);
+  }
+
+  function showMobileMenu() {
+    if (navMenuMobile) {
+      navMenuMobile.setAttribute("data-mobile-visibility", "shown");
+    }
+
+    if (closeMobileMenuButton) {
+      closeMobileMenuButton.setAttribute("data-mobile-visibility", "shown");
+    }
+  }
+
+  function hideMobileMenu() {
+    if (navMenuMobile) {
+      navMenuMobile.setAttribute("data-mobile-visibility", "hidden");
+    }
+  }
 })(document);
